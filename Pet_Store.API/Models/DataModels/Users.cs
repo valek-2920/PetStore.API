71b870dc408ed249a14c76112fe56ec6b3a5c6b3
Valeria Morales <78573287+valek-2920@users.noreply.github.com>
2022-07-11 16:20:20 -0600

    Revert "Create Repositories, Unity of Work and Category Controller"

---
+++ Pet_Store.API/Models/DataModels/Users.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_PetStore.API.Models.DataModels
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public int Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        /*Foreign keys*/
+        [Required]
+        public int RolesId { get; set; }
+
+        [ForeignKey("RolesId")]
+        [ValidateNever]
+        public UserRoles Role { get; set; }
+
+        [Required]
+        public int DirectionId { get; set; }
+
+        [ForeignKey("DirectionId")]
+        [ValidateNever]
+        public UserDirection Directions { get; set; }
+
+
+    }
+}

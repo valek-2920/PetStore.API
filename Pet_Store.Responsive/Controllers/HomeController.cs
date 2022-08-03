15fddb5ed23d3037b94266d5c93ce66ae5894733
Jurgen <jurgen1138@gmail.com>
2022-08-03 02:19:25 -0600

    Add: Views of Index,Login and register. Also added and removed some libraries

--- Pet_Store.Responsive/Controllers/HomeController.cs
+++ Pet_Store.Responsive/Controllers/HomeController.cs
@@ -33,5 +33,16 @@ namespace Pet_Store.Responsive.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+
+            return View();
+        }
     }
 }
